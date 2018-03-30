820ea810103bf306b79f70de6ec132748b6c9a4a
diegoferreira <dgnnsx@gmail.com>
2018-03-30 14:35:15 -0300

    [FPGA-DRIVER] Saida de dados

--- src/arduino/fpga_driver/fpga_driver.ino
+++ src/arduino/fpga_driver/fpga_driver.ino
@@ -1,24 +1,51 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
+const int output[] = {
+    22, 24, 26, 28, 30, 32, 34, 36
+};
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+void setup() {
+    // Inicializa as saidas para o fpga
+    for (int i = 0; i < 8; i++) {
+        pinMode(output[i], OUTPUT);
+    }
+    
+    // Inicializa a comunicaçao serial
+    Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);               // wait for a second
-}
+    if (Serial.available() > 0) {
+        int inByte = Serial.read();
+       
+        switch (inByte) {
+            case '1':
+                digitalWrite(output[0], HIGH);
+                break;
+            case '2':
+                digitalWrite(output[1], HIGH);
+                break;
+            case '3':
+                digitalWrite(output[2], HIGH);
+                break;
+            case '4':
+                digitalWrite(output[3], HIGH);
+                break;
+            case '5':
+                digitalWrite(output[4], HIGH);
+                break;
+            case '6':
+                digitalWrite(output[5], HIGH);
+                break;
+            case '7':
+                digitalWrite(output[6], HIGH);
+                break;
+            case '8':
+                digitalWrite(output[7], HIGH);
+                break;
+            default:
+                // Atribui LOW para todas saidas
+                for (int i = 0; i < 8; i++) {
+                    digitalWrite(output[i], LOW);
+                }
+        }
+    }
+} 
