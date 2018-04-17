e2cb2981536a89f9fbe476056a311abefb902c03
Davi Morales <davimmorales@gmail.com>
2018-04-17 20:01:19 -0300

    fpga output converted from binary to decimal

--- src/arduino/fpga_driver/fpga_driver.ino
+++ src/arduino/fpga_driver/fpga_driver.ino
@@ -1,25 +1,28 @@
-// Pinos utilizados para entrada no arduino
+// Quantidade de pinos utilizados para entrada/saida
+const int quantidadePinos = 8;
+
+// Limiar para conversao da entrada analogica para sinal digital
+const int limiarAnalogicoParaDigital = 1000;
+
+// Pinos utilizados para entrada analogica no Arduino
 const int input[] = {
     8, 9, 10, 11, 12, 13, 14, 15
 };
 
-// Pinos utilizados para saida no arduino
+// Pinos utilizados para saida digital do Arduino
 const int output[] = {
     24, 26, 28, 30, 32, 34, 36, 38
 };
 
-int states[8];
-int state = 0;
-
 void setup() {
-    // Inicializa as saidas para o fpga
-    for (int i = 0; i < 8; i++) {
+    // Inicializa as saidas para o FPGA
+    for (int i = 0; i < quantidadePinos; i++) {
         pinMode(input[i], INPUT);
         pinMode(output[i], OUTPUT);
     }
-
-    // Inicializa a comunicaçao serial
+    // Inicializa a comunicacao serial
     Serial.begin(9600);
+    // Inicializa referencia analogica
     analogReference(INTERNAL1V1);
 }
 
@@ -59,7 +62,7 @@ void toFPGA() {
                 break;
             default:
                 // Atribui LOW para todas saidas
-                for (int i = 0; i < 8; i++) {
+                for (int i = 0; i < quantidadePinos; i++) {
                     digitalWrite(output[i], LOW);
                 }
         }
@@ -67,8 +70,21 @@ void toFPGA() {
 }
 
 void fromFPGA() {
-    for (int i = 7; i >= 0; i--) {
-        Serial.print(analogRead(input[i]) > 1000 ? '1' : '0');
+    Serial.println(binaryToDecimal());
+}
+
+int binaryToDecimal() {
+    int decimal = 0;
+    int potencia = 1;
+    for (int i = 0; i < quantidadePinos; i++) {
+        if (analogToDigital(i)) {
+            decimal += potencia;
+        }
+        potencia *= 2;
     }
-    Serial.println();
+    return decimal;
+}
+
+int analogToDigital(int index) {
+    return analogRead(input[index]) >= limiarAnalogicoParaDigital ? 1 : 0;
 }
