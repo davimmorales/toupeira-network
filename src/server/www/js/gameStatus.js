0f498c6d46d0b3d87a1bdb9a5fa9bea83d1375af
Rodrigo de Farias Ramires <rodrigoramiresbr94@gmail.com>
2018-05-27 12:33:25 -0300

    game page finished

---
+++ src/server/www/js/gameStatus.js
@@ -0,0 +1,47 @@
+(() => {
+
+  const initialize = () => {
+    $('#startGameButton').click(startGame);
+  }
+
+  const startGame = (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    $.ajax({
+      url: '/game/start',
+      type: 'POST',
+      data: {startGame: true},
+      success: () => {
+        enableSelectPlayerArea(true);
+      },
+      error: () => { console.log('Start game failed'); }
+    });
+  }
+
+  const refreshPage = () => {
+    $.ajax({
+      url: '/game/status',
+      type: 'GET',
+      dataType: 'json',
+      success: (jsonData) => {
+        enableSelectPlayerArea(jsonData.status.inProgress);
+      },
+      error: () => { console.log('Refresh failed'); }
+    });
+  }
+
+  const enableSelectPlayerArea = (inProgress) => {
+    if (inProgress === true) {
+      document.getElementById('startGame').setAttribute('class', 'no-show');
+      document.getElementById('selectPlayer').setAttribute('class', '');
+    } else {
+      document.getElementById('startGame').setAttribute('class', '');
+      document.getElementById('selectPlayer').setAttribute('class', 'no-show');
+    }
+  }
+
+  $(document).ready(initialize);
+  $(document).ready(window.setInterval(refreshPage, 1000));
+
+})();
